e0aae71117eca6d591ffc701528b845e4ab95024
developersihyeon <139722653+developersihyeon@users.noreply.github.com>
2024-07-18 00:58:38 +0900

    feat :: 글작성 뷰 완성

--- src/components/router/index.tsx
+++ src/components/router/index.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "src/page/auth/login/page";
 import SignUpPage from "src/page/auth/signup/page";
 import MainPage from "src/page/main/page";
+import WritePage from "src/page/write/page";
 import PreparePage from "src/page/prepare/page";
 
 const Router = () => {
@@ -11,6 +12,7 @@ const Router = () => {
       <Route path="login" element={<LoginPage />} />
       <Route path="/sign-up" element={<SignUpPage />} />
       <Route path="/" element={<MainPage />} />
+      <Route path="/write" element={<WritePage />} />
 
       <Route path="/company" element={<PreparePage />} />
       <Route path="/rest" element={<PreparePage />} />
